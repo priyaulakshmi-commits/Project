b3f45f53c6a724998dc3dc9b78ced106b34b04d3
PRIYA <priyaulakshmi@gmail.com>
2026-02-06 20:01:43 +0530

    cleanup

--- src/com/airtribe/learntrack/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.airtribe.learntrack.exception;
-
-public class EntityNotFoundException extends Exception {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
